238a03b1a1187f03156edf6977d53a1ea5b3bf89
Priya <priyadharshinirajendran1701@gmail.com>
2026-09-07 13:11:52 +0530

    Day 6 - Complete Python Problem 10

---
+++ python/day6/q10.py
@@ -0,0 +1,9 @@
+#Find the largest in Tuple.
+#find the largest number without using max().
+
+numbers=(10, 45, 23, 67, 12)
+largest=numbers[0]
+for num in numbers:
+    if num>largest:
+        largest=num
+print("Largest:",largest)
