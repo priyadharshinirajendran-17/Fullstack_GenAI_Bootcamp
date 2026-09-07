38899f3f2f149d2df899267dac3c077ebd376c2f
Priya <priyadharshinirajendran1701@gmail.com>
2026-09-07 12:27:49 +0530

    Day 6 - Completed python Problem 1

---
+++ python/day6/q1.py
@@ -0,0 +1,10 @@
+#Q1 — Tuple Create a tuple containing:
+#Name, Age, Course, City
+#Then unpack it into four variables and print each value.
+
+student=("Priya", 24, "MCA", "Trichy")
+name, age, course, city=student
+print("Name:",name)
+print("Age:",age)
+print("Course:",course)
+print("City:",city)
