c07723c038ee599ad2ad2f6be756a526c95986dc
Priya <priyadharshinirajendran1701@gmail.com>
2026-09-07 12:28:34 +0530

    Day 6 - Completed python Problem 2

---
+++ python/day6/q2.py
@@ -0,0 +1,7 @@
+#Q2 — Remove Duplicates
+#Given: numbers = [1, 2, 2, 3, 4, 4, 5, 5]
+#Create a set and print the unique values.
+
+numbers = [1, 2, 2, 3, 4, 4, 5, 5]
+unique_numbers=set(numbers)
+print(unique_numbers)
