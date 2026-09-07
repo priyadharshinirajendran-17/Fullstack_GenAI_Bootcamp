d61ff39f62ccc97cabb63e6d5389e8a13e9f54f2
Priya <priyadharshinirajendran1701@gmail.com>
2026-09-07 13:13:39 +0530

    Day 6 - Complete Python Problem 12

---
+++ python/day6/q12.py
@@ -0,0 +1,6 @@
+#Convert list -> Tuple. Given: numbers=[10, 20, 30, 40]
+#Convert it into a tuple. Expected output: (10, 20, 30, 40)
+
+numbers=[10, 20, 30, 40]
+numbers_tuple=tuple(numbers)
+print(numbers_tuple)
